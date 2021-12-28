d69176ce2296d8dcf5491cf6f8444398ac3d3a80
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2021-12-29 06:51:02 +0800

    design timenline

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,7 +1,35 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <x-container>
+        <div class="grid grid-cols-12 gap-5">
+            <div class="col-span-8">
+                <div class="space-y-5">
+                    @foreach ($statuses as $status)
+                        <div class="flex">
+                            <div class="flex-shrink-0s mr-3">
+                                <img class="w-10 h-10 rounded-full mr-3" src="https://i.pravatar.cc/150" alt="">
+                            </div>
+    
+                            <div>
+                                <div class="font-semibold">
+                                    {{ ucwords($status->user->name) }}
+                                </div>
+    
+                                <div>
+                                    {{ $status->body}}
+                                </div>
+    
+                                <div class="text-gray-500 text-sm">
+                                    {{-- {{ $status->created_at->format('d M, y') }} --}}
+                                    {{ $status->created_at->diffforhumans() }}
+                                </div>
+                            </div>
+                        </div>    
+                    @endforeach
+                </div>
+            </div>
+            <div class="col-span-4">
+                friend
+            </div>
+        </div>
+    </x-container>
 </x-app-layout>
